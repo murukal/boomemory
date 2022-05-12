edc8c4a66be406197d14aeb64458bee4f5f5073e
murukal <jokerxxx222@126.com>
2022-05-12 09:12:21 +0800

    jwt secret设置环境变量默认值

--- apps/boomemory/src/config/jwt.config.ts
+++ apps/boomemory/src/config/jwt.config.ts
@@ -2,5 +2,5 @@ import { registerAs } from '@nestjs/config';
 import { v4 } from 'uuid';
 
 export const jwtConfig = registerAs('jwt', () => ({
-  secret: v4(),
+  secret: process.env.JWT_SECRET || v4(),
 }));
