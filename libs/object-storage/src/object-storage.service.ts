4c058090ce1388aee3c6821eb3332c3f9b80d669
murukal <jokerxxx222@126.com>
2022-06-07 08:57:16 +0800

    feat: 移除代码中的对象存储秘钥信息，使用环境变量

--- libs/object-storage/src/object-storage.service.ts
+++ libs/object-storage/src/object-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import COS = require('cos-nodejs-sdk-v5');
 import type { UploadBody } from 'cos-nodejs-sdk-v5';
 import { v4 } from 'uuid';
+import { ConfigService } from '@app/config';
 
 export interface FileProfile {
   body: UploadBody;
@@ -13,17 +14,21 @@ export interface FileProfile {
 export class ObjectStorageService {
   private readonly cos: COS;
 
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
+    // 初始化对象存储
     this.cos = new COS({
-      SecretId: 'AKIDfAOhlY4JGxNWOzJM7cqpWx70cwzlGUS8',
-      SecretKey: 'QZR3OSFm20h7G9l59wRfdKkW3IHzQbU6',
+      SecretId: this.configService.getCosSecretId(),
+      SecretKey: this.configService.getCosSecretKey(),
     });
   }
 
+  /**
+   * 上传附件
+   */
   async upload2COS(fileProfile: FileProfile) {
     const res = await this.cos.putObject({
-      Bucket: 'boomemory-1304340057',
-      Region: 'ap-shanghai',
+      Bucket: this.configService.getCosBucket(),
+      Region: this.configService.getCosRegion(),
       Key: fileProfile.filename || v4(),
       ContentType: fileProfile.mimetype,
       Body: fileProfile.body,
