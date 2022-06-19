7a82cdcdc698206341951bebe2c525ec48b2f6c1
murukal <jokerxxx222@126.com>
2022-06-19 21:07:43 +0800

    feat: 修改默认端口

--- libs/config/src/assets/port.config.ts
+++ libs/config/src/assets/port.config.ts
@@ -4,9 +4,9 @@ import { AppID } from 'utils/app/assets';
 export const portConfig = registerAs('port', () => {
   // 子项目监听的端口不同，利用环境变量控制
   return {
-    [AppID.Bot]: process.env.BOT_PORT || 3000,
-    [AppID.Boomemory]: process.env.BOOMEMORY_PORT || 3100,
-    [AppID.Boomart]: process.env.BOOMART_PORT || 3200,
-    [AppID.Boomoney]: process.env.BOOMONEY_PORT || 3300,
+    [AppID.Bot]: process.env.BOT_PORT || 8000,
+    [AppID.Boomemory]: process.env.BOOMEMORY_PORT || 8100,
+    [AppID.Boomart]: process.env.BOOMART_PORT || 8200,
+    [AppID.Boomoney]: process.env.BOOMONEY_PORT || 8300,
   };
 });
